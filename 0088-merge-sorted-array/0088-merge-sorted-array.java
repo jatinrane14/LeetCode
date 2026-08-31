cfdbb10d5516ddbd336974dfba23126a251e675d
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2026-08-31 12:41:49 +0530

    Time: 3 ms (22.7%), Space: 43.8 MB (63.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,29 @@
-class Solution{
+class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+        int left = m-1;
+        int right = 0;
+        while(left>=0 && right < n){
+    
+            if(nums1[left] > nums2[right]){
+                int temp = nums1[left];
+                nums1[left] = nums2[right];
+                nums2[right] = temp;
+                left--;
+                right++;
+            }else{
+                break;
+            }
         }
+        
+        int j = m;
+
+        for (int i = 0; i < n; i++) {
+            nums1[j] = nums2[i];
+            j++;
+        }
+
         Arrays.sort(nums1);
+
+
     }
 }
