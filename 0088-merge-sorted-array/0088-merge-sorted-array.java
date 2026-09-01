182b82302103c6a554c7300b7a22bf2d8f7d0698
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2026-09-01 11:30:43 +0530

    Time: 1 ms (26.5%), Space: 43.7 MB (63.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,29 +1,38 @@
 class Solution {
+    private void swap(int[] nums1, int[] nums2,int indx1, int indx2){
+        if(nums1[indx1] > nums2[indx2]){
+            int temp = nums1[indx1];
+            nums1[indx1]  = nums2[indx2];
+            nums2[indx2] = temp;
+        }
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int left = m-1;
-        int right = 0;
-        while(left>=0 && right < n){
-    
-            if(nums1[left] > nums2[right]){
-                int temp = nums1[left];
-                nums1[left] = nums2[right];
-                nums2[right] = temp;
-                left--;
-                right++;
-            }else{
-                break;
+        int length = m+n;
+        int gap = (length/2) + (length%2);
+        while(gap > 0){
+            int left = 0;
+            int right = left+gap;
+            while(right<length){
+                // arr1 and arr2
+                if(left<m && right >=m){
+                    swap(nums1,nums2,left,right-m);
+                }
+                // arr2 and arr2
+                else if(left>=m){
+                    swap(nums2,nums2,left-m,right-m);
+                }
+                else{
+                    swap(nums1,nums1,left,right);
+                }
+                left++;right++;
             }
+            if(gap ==1) break;
+            gap = (gap/2) + (gap%2);
         }
-        
-        int j = m;
-
-        for (int i = 0; i < n; i++) {
-            nums1[j] = nums2[i];
-            j++;
+        int ind = m; 
+        for(int i =0;i<n;i++){
+            nums1[ind] = nums2[i];
+            ind++;
         }
-
-        Arrays.sort(nums1);
-
-
     }
 }
